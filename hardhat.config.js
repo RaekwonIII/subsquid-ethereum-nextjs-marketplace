e230f39bb19c83e68842c010a6bf241ad4e817da
Massimo Luraschi <massimo.lura@gmail.com>
2023-01-09 11:25:51 +0100

    Updated README with improved instructions Removed mentions of config JS file Removed config.js example Commented Goerli network in hardhat so it will always start Added example environment file Fixed Infura API URL Added env variable for Infura Dedicated Gateway

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-const infuraId = fs.readFileSync(".infuraid").toString().trim() || ""; // process.env.INFURA_NODE_ID;//
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || ""; // process.env.INFURA_NODE_ID;//
 
-const account = fs.readFileSync(".account").toString().trim() || "";
+// const account = fs.readFileSync(".account").toString().trim() || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -14,11 +14,11 @@ module.exports = {
       //   interval: [4800, 5200]
       // }
     },
-    goerli: {
-      // Infura
-      url: `https://goerli.infura.io/v3/${infuraId}`,
-      accounts: [account]
-    },
+    // goerli: {
+    //   // Infura
+    //   url: `https://goerli.infura.io/v3/${infuraId}`,
+    //   accounts: [account]
+    // },
     /*
     matic: {
       // Infura
